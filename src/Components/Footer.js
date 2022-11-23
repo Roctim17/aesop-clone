9d94859e3e55f50ee7a6bbaaa105232f8e3e1c94
roctim17m <mdrokibulislamroctim@gmail.com>
2022-11-24 00:25:14 +0600

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,102 +1,98 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/Footer.css'
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Footer = () => {
     return (
         <>
-            <div className='footer'>
-                <div className='p-5 m-0'>
-                    <div className='row '>
-                        <div className='col-lg-5'>
-                            <div class="input-group">
-                                <span class="input-group-text w-100 d-flex justify-content-between"> <span>Email address </span> <i class="fas fa-arrow-right"></i></span>
-                            </div>
-                            <div>
-                                <div class="form-check py-3">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                    Subscribe to receive communications from Aesop about our products and services. By subscribing, you confirm you have read and accept our <br/> <Link>privacy policy </Link>
-                                    </label>
-                                </div>
-                               
-                            </div>
+            <div className='footer bg-dark'>
+                <div className='footer-one p-5 m-0'>
 
+                    <div className='one email'>
+                        <div class="input-group">
+                            <span class="input-group-text bg-dark text-light w-100 d-flex justify-content-between"> <span>Email address </span> <AiOutlineArrowRight/> </span>
                         </div>
-                        <div className='col-lg-2'>
-                            <h6>Orders and support</h6>
-                            <hr />
-                            <ul>
-                                <li>Contact us</li>
-                                <li>FAQs </li>
-                                <li>Shipping </li>
-                                <li>Returns </li>
-                                <li>Order history</li>
-                                <li>Terms and conditions</li>
-                            </ul>
-                        </div>
-                        <div className='col-lg-2'>
-                            <h6>Services</h6>
-                            <hr />
-                            <ul>
-                                <li>Live assistance</li>
-                                <li>Corporate gifts</li>
-                                <li>Facial appointments</li>
-                                <li>Click and Collect</li>
-                                <li>Video consultation</li>
-                            </ul>
-                        </div>
-                        <div className='col-lg-2'>
-                            <h6>Location preferences</h6>
-                            <hr />
-                            <ul>
-                                <li>Shipping:</li>
-                                <li>Hong Kong SAR, China</li>
-                                <li></li>
-                                <li>Language:</li>
-                                <li>English</li>
-                                <li>繁體中文</li>
-                                <li>简体中文</li>
-                            </ul>
+                        <div>
+                            <div class="form-check py-3">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Subscribe to receive communications from Aesop about our products and services. By subscribing, you confirm you have read and accept our <br /> <Link>privacy policy </Link>
+                                </label>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='px-5'>
-                    <div className='row'>
-                        <div className='col-lg-5'>
-                            <h6>Sustainability</h6>
-                            <hr />
-                            <p>All Aesop products are vegan, and we do not test our formulations or ingredients on animals. We are Leaping Bunny approved and a Certified B Corporation. Learn more</p>
-                        </div>
-                        <div className='col-lg-2'>
-                            <h6>About</h6>
-                            <hr />
-                            <ul>
-                                <li>Our story</li>
-                                <li>Foundation</li>
-                                <li>Careers</li>
-                                <li>Privacy policy</li>
-                                <li>Accessibility</li>
-                                <li>Cookie Policy</li>
-                            </ul>
-                        </div>
-                        <div className='col-lg-2'>
-                            <h6>Social media</h6>
-                            <hr />
-                            <ul>
-                                <li>Instagram </li>
-                                <li>Twitter </li>
-                                <li>LinkedIn </li>
-                                <li>WeChat</li>
-                                <li>Weibo </li>
-                            </ul>
-                        </div>
+                    <div className='one Orders'>
+                        <h6>Orders and support</h6>
+                        <p className='before'></p>
+                        <p>Contact us</p>
+                        <p>FAQs </p>
+                        <p>Shipping </p>
+                        <p>Returns </p>
+                        <p>Order history</p>
+                        <p>Terms and conditions</p>
+
                     </div>
+
+                    <div className='one Services'>
+                        <h6>Services</h6>
+                        <p className='before'></p>
+                        <p>Live assistance</p>
+                        <p>Corporate gifts</p>
+                        <p>Facial appointments</p>
+                        <p>Click and Collect</p>
+                        <p>Video consultation</p>
+
+                    </div>
+
+                    <div className='one Location'>
+                        <h6>Location preferences</h6>
+                        <p className='before'></p>
+                        <p>Shipping:</p>
+                        <p>Hong Kong SAR, China</p>
+                        <p></p>
+                        <p>Language:</p>
+                        <p>English</p>
+                        <p>繁體中文</p>
+                        <p>简体中文</p>
+                    </div>
+
+
+
+                    <div className='one Sustainability'>
+                        <h6>Sustainability</h6>
+                        <p className='before'></p>
+                        <p>All Aesop products are vegan, and we do not test our formulations or ingredients on animals. We are Leaping Bunny approved and a Certified B Corporation. Learn more</p>
+                    </div>
+
+                    <div className='one About'>
+                        <h6>About</h6>
+                        <p className='before'></p>
+                        <p>Our story</p>
+                        <p>Foundation</p>
+                        <p>Careers</p>
+                        <p>Privacy policy</p>
+                        <p>Accessibility</p>
+                        <p>Cookie Policy</p>
+
+                    </div>
+
+                    <div className='one Social'>
+                        <h6>Social media</h6>
+                        <p className='before'></p>
+                        <p>Instagram </p>
+                        <p>Twitter </p>
+                        <p>LinkedIn </p>
+                        <p>WeChat</p>
+                        <p>Weibo </p>
+
+                    </div>
+
                 </div>
 
             </div>
             <hr className='m-0' />
-            <div className='px-5 aesop'> <p className='py-3'>© Aesop</p></div>
+            <div className='footer-three px-5 aesop'> <p className='py-3'>© Aesop</p></div>
         </>
     );
 };
